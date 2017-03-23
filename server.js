8154cf2cf9fc6c3a9ba48b66b30b05b656e97139
Danielle Bowman <dcbowman6@yahoo.com>
2017-03-23 14:08:59 -0400

    connects to db and main html set

---
+++ server.js
@@ -0,0 +1,32 @@
+var Model = require('./models/models.js');
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+var db = "mongodb://localhost/mean_end_to_end";
+
+mongoose.connect(db, function(err, response){
+	if(err){
+		console.log('Failed to connect to ' + db);
+	}
+	else{
+		console.log('Connected to ' + db);
+	}//lets user know if connection to db is successful
+});
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended:true }));
+
+app.use(morgan('dev')); //logger
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function(){
+	console.log("Listening on port 3000");
+})
+
